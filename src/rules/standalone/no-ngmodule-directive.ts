a7a0de74838a0091c04d4ce597e4f60b07df4012
Cyrille Tuzi <555867+cyrilletuzi@users.noreply.github.com>
2026-08-12 16:48:03 +0200

    feat: add no-ngmodule-directive rule

---
+++ src/rules/standalone/no-ngmodule-directive.ts
@@ -0,0 +1,37 @@
+import type { RuleDefinition } from "@eslint/core";
+import { AST_NODE_TYPES, type TSESTree } from "@typescript-eslint/utils";
+import { getDecoratorProperty } from "../../utils/ast-decorator-property";
+
+export const ruleName = "no-ngmodule-directive";
+const messageId = "noNgmoduleDirective";
+
+export const ruleDefinition: RuleDefinition = {
+  meta: {
+    type: "problem",
+    messages: {
+      [messageId]: `\`standalone: false\` creates a NgModule-based directive, use a standalone directive instead.`,
+    },
+    docs: {
+      description: `Checks that \`standalone: false\` is not used in a directive.`,
+      url: 'https://github.com/cyrilletuzi/eslint-plugin-angular-modern/blob/main/docs/standalone/NO_NGMODULE_DIRECTIVE.md',
+    },
+    schema: [],
+  },
+  create(context) {
+    return {
+      "Decorator[expression.callee.name='Directive']"(node: TSESTree.Decorator) {
+        const standalone = getDecoratorProperty(node, "standalone");
+
+        if (
+          standalone?.value.type === AST_NODE_TYPES.Literal &&
+          standalone.value.value === false
+        ) {
+          context.report({
+            node: standalone,
+            messageId,
+          });
+        }
+      },
+    };
+  },
+};
