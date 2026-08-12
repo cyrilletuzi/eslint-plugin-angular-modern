cf40285bbdb2c94ef7f83afaed7ee076e67d6df5
Cyrille Tuzi <555867+cyrilletuzi@users.noreply.github.com>
2026-08-12 14:02:27 +0200

    feat: add no-browseranimationsmodule rule

---
+++ src/rules/standalone/no-browseranimationsmodule.ts
@@ -0,0 +1,33 @@
+import type { RuleDefinition } from "@eslint/core";
+import type { TSESTree } from "@typescript-eslint/utils";
+import { isImportIdentifier } from "../../utils/is-import-identifier";
+
+export const ruleName = "no-browseranimationsmodule";
+const messageId = "noBrowseranimationsmodule";
+
+export const ruleDefinition: RuleDefinition = {
+  meta: {
+    type: "problem",
+    messages: {
+      [messageId]: `\`BrowserAnimationsModule\` should be avoided in a standalone application, is deprecated and is planned for removal; in the meantime, use \`provideAnimations()\` or \`provideAnimationsAsync()\` instead.`,
+    },
+    docs: {
+      description: `Checks that \`BrowserAnimationsModule\` is not used.`,
+      url: 'https://github.com/cyrilletuzi/eslint-plugin-angular-modern/blob/main/docs/standalone/NO_BROWSERANIMATIONSMODULE.md',
+      recommended: true,
+    },
+    schema: [],
+  },
+  create(context) {
+    return {
+      "Identifier[name='BrowserAnimationsModule']"(node: TSESTree.Identifier) {
+        if (!isImportIdentifier(node)) {
+          context.report({
+            node,
+            messageId,
+          });
+        }
+      },
+    };
+  },
+};
