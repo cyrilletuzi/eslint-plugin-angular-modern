04fb490a94a11222c2be46e39f7b522872e2ef80
Cyrille Tuzi <555867+cyrilletuzi@users.noreply.github.com>
2026-08-12 14:04:11 +0200

    feat: add no-httpclienttestingmodule rule

---
+++ src/rules/standalone/no-httpclienttestingmodule.ts
@@ -0,0 +1,33 @@
+import type { RuleDefinition } from "@eslint/core";
+import type { TSESTree } from "@typescript-eslint/utils";
+import { isImportIdentifier } from "../../utils/is-import-identifier";
+
+export const ruleName = "no-httpclienttestingmodule";
+const messageId = "noHttpclienttestingmodule";
+
+export const ruleDefinition: RuleDefinition = {
+  meta: {
+    type: "problem",
+    messages: {
+      [messageId]: `\`HttpClientTestingModule\` should be avoided in a standalone application and is deprecated, use \`provideHttpClientTesting()\` instead.`,
+    },
+    docs: {
+      description: `Checks that \`HttpClientTestingModule\` is not used.`,
+      url: 'https://github.com/cyrilletuzi/eslint-plugin-angular-modern/blob/main/docs/standalone/NO_HTTPCLIENTTESTINGMODULE.md',
+      recommended: true,
+    },
+    schema: [],
+  },
+  create(context) {
+    return {
+      "Identifier[name='HttpClientTestingModule']"(node: TSESTree.Identifier) {
+        if (!isImportIdentifier(node)) {
+          context.report({
+            node,
+            messageId,
+          });
+        }
+      },
+    };
+  },
+};
